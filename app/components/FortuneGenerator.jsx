2e4877bd95b994d00fae8653f240c2154792b8ee
Nikolas Nemergut <165232513+NemergutN@users.noreply.github.com>
2024-09-19 22:12:39 -0400

    page is setup, fortune still not generating...

--- app/components/FortuneGenerator.jsx
+++ app/components/FortuneGenerator.jsx
@@ -4,19 +4,19 @@ import { useEffect, useState } from "react"
 function FortuneGenerator() {
     const [fortunes, fortuneData] = useState([])
 
-    async function generateFortune(){
-        const res = await fetch ("http://localhost:8080/fortunes", {
+    async function generateFortune() {
+        const res = await fetch("http://localhost:8080/fortunes", {
             method: "GET",
         })
         const resJSON = await res.json()
         fortuneData(resJSON)
-        }
+    }
 
     useEffect(() => {
         generateFortune()
     }, [])
 
-    return(
+    return (
         <div>
             <center>
                 <button onClick = {generateFortune}>
@@ -24,13 +24,15 @@ function FortuneGenerator() {
                 </button>
             </center>
             <h2>Your Fortune:</h2>
-            {fortunes.map((fortune) => {
-                return(
-                    <h1>
-                        {fortune.fortuneList.at(Math.floor(Math.random()*3))}
-                    </h1> // WHY DOES THIS NOT WORK!!!!!
+            {
+                fortunes.map((fortune) =>
+                    <center>
+                        <h1 key={fortune.id}>
+                            {fortune.fortuneList.at(Math.floor(Math.random()*3))}
+                        </h1>
+                    </center> // Why no work :(
                 )
-            })}
+            }
         </div>
     )
 }
